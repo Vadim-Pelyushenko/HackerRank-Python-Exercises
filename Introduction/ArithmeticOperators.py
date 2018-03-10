41cbde44fc8c6b6b7487d5f467481b98caf2e525
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-09 23:03:46 -0800

    Using Arithmetic Operations

---
+++ Introduction/ArithmeticOperators.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+	a = int(input())
+	b = int(input())
+	sum = a + b
+	diff = a - b
+	prod = a * b
+	print(str(sum))
+	print(str(diff))
+	print(str(prod))
